a0537070d4541b1a47c72eff2be697b7aa8746b9
redassser <redryan890@gmail.com>
2023-11-19 12:50:48 -0500

    first release

--- Plugin.cs
+++ Plugin.cs
@@ -1,14 +1,56 @@
 ﻿using BepInEx;
+using HarmonyLib;
+using GameNetcodeStuff;
+using UnityEngine.InputSystem;
 
 namespace Walkie
 {
-    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
-    public class Plugin : BaseUnityPlugin
+    [BepInPlugin("rr.Walkie", "WalkieUse", "1.0.0")]
+    [HarmonyPatch(typeof(PlayerControllerB))]
+    public class WalkieToggle : BaseUnityPlugin
     {
+        private Harmony _harmony = new Harmony("Flashlight");
         private void Awake()
         {
-            // Plugin startup logic
-            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            this._harmony.PatchAll(typeof(WalkieToggle));
+            this.Logger.LogInfo("------Walkie done.------");
+        }
+        [HarmonyPatch(typeof(PlayerControllerB), "Update")]
+        [HarmonyPostfix]
+        public static void ReadInput(PlayerControllerB __instance)
+        {
+            GrabbableObject pocketWalkie = null;
+            if ((!__instance.IsOwner || !__instance.isPlayerControlled || __instance.IsServer && !__instance.isHostPlayerObject) && !__instance.isTestingPlayer)
+                return;
+            for (int index = 0; index < __instance.ItemSlots.Length; ++index)
+            {
+                if (__instance.ItemSlots[index] is WalkieTalkie && __instance.ItemSlots[index].isBeingUsed)
+                {
+                    pocketWalkie = __instance.ItemSlots[index];
+                    break;
+                }
+            }
+            if (pocketWalkie == null) return;
+            if (Keyboard.current.tKey.wasPressedThisFrame)
+            {
+                if (__instance.currentlyHeldObjectServer is WalkieTalkie)
+                {
+                    __instance.currentlyHeldObjectServer.UseItemOnClient(true);
+                } else if (pocketWalkie != null)
+                {
+                    pocketWalkie.UseItemOnClient(true);
+                }
+            }
+            if (Keyboard.current.tKey.wasReleasedThisFrame)
+            {
+                if (__instance.currentlyHeldObjectServer is WalkieTalkie)
+                {
+                    __instance.currentlyHeldObjectServer.UseItemOnClient(false);
+                } else if (pocketWalkie != null)
+                {
+                    pocketWalkie.UseItemOnClient(false);
+                }
+            }
         }
     }
 }
---
+++ WalkieUse/README.md
@@ -0,0 +1,10 @@
+
+# Walkie Use
+
+## Press T to use the first ON walkie talkie in your inventory
+
+Works if you are holding another item or the walkie talkie itself.
+
+All players must have the mod installed. (maybe havent tested. server owner for sure though)
+
+Walkie talkie must be turned on first.
